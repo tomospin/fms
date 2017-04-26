5b92ade0eeb59dce8d10321fe8b519b2c02ee30b
sirapat <tomospin@gmail.com>
2017-04-26 11:54:08 +0700

    first commit

---
+++ client/fms/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+ 
+import { LoginPage } from '../pages/login-page/login-page';
+ 
+@Component({
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
+})
+export class MyApp {
+  rootPage = LoginPage;
+ 
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // StatusBar.styleDefault();
+    });
+  }
+}
---
+++ client/fms/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { IonicStorageModule } from '@ionic/storage'
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login-page/login-page';
+import { SignupPage } from '../pages/signup-page/signup-page';
+import { Todos } from '../providers/todos';
+import { Auth } from '../providers/auth';
+ 
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicStorageModule.forRoot(),
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  providers: [Storage, Todos, Auth]
+})
+export class AppModule {}
